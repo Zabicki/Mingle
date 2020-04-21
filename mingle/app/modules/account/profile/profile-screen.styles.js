af5ea49a1a5744c27009967a7e020d3d1d90b0b7
KlaudiaK1998 <62907563+KlaudiaK1998@users.noreply.github.com>
2020-04-21 12:32:09 +0200

    Feature/frontend/profile settings (#15)
    * Test
    
    * Test
    
    * Tst
    
    * Settings screen
    
    * Settings screen

---
+++ mingle/app/modules/account/profile/profile-screen.styles.js
@@ -0,0 +1,90 @@
+import { StyleSheet } from 'react-native'
+
+import { Metrics, ApplicationStyles, Colors } from '../../../shared/themes/'
+
+export default StyleSheet.create({
+  ...ApplicationStyles.screen,
+  container: {
+    flex: 1,
+    backgroundColor: "#FFF"
+  },
+  textName: {
+    color: Colors.sun,
+    fontSize: 36,
+    fontWeight: "200",
+    padding: 5
+    
+  },
+  textAge: {
+    fontWeight: "bold",
+    color: "#AEB5BC", 
+    fontSize: 18,
+    padding: 0  
+  },
+  textLocation: {
+    color: "#AEB5BC", 
+    fontSize: 18,
+    padding: 0  
+  },
+  buttonSection: {
+    marginTop: 10,
+    alignItems: 'center',
+  },
+  scrollView: {
+    paddingBottom: Metrics.baseMargin,
+    backgroundColor: 'white',
+  },
+  infoContainer: {
+    alignSelf: "center",
+    alignItems: "center",
+    margin: 5,
+  },
+  image: {
+    flex: 1,
+    height: undefined,
+    width: undefined
+  },
+  profileImage: {
+    marginTop: 48,
+    alignSelf: "center",
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+    overflow: "hidden"
+  },
+ 
+  scrollView: {
+    paddingBottom: Metrics.baseMargin,
+    backgroundColor: 'white',
+  },
+  body: {
+    backgroundColor: 'white',
+  },
+  button: {
+    flex: 1,
+    height: 40,
+    width: 115,
+    padding: 6,
+    backgroundColor: Colors.sun,
+    borderColor: Colors.sun,
+    borderWidth: 1,
+    borderRadius: 3,
+    justifyContent: 'center',
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: '600',
+    color: 'white',
+    alignSelf: 'center',
+  },
+  textDescription: {
+    width: 350,
+    height: 110,
+    fontSize: 18,
+    color: "#AEB5BC",
+  },
+  description: {
+    height: 150,
+    alignItems: 'center',
+  },
+})

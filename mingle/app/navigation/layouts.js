b7d316eea15995ef46e791b790c9f3078a01a913
KlaudiaK1998 <klaudiaklepacka1998@gmail.com>
2020-05-23 23:01:32 +0200

    Add event info to menu

--- mingle/app/navigation/layouts.js
+++ mingle/app/navigation/layouts.js
@@ -12,11 +12,11 @@ import LoginScreen from '../modules/login/login-screen'
 import LaunchScreen from '../modules/home/launch-screen'
 import DrawerContent from './drawer/drawer-content'
 import SettingsScreen from '../modules/account/settings/settings-screen'
-import PreferencesScreen from '../modules/account/preferences/preferences-screen'
 import RegisterScreen from '../modules/account/register/register-screen'
 import ForgotPasswordScreen from '../modules/account/password-reset/forgot-password-screen'
 import ChangePasswordScreen from '../modules/account/password/change-password-screen'
 import ProfileScreen from '../modules/account/profile/profile-screen'
+import EventInfoScreen from '../modules/entities/event-info-screen'
 import EntitiesScreen from '../modules/entities/entities-screen'
 import StorybookScreen from '../../storybook'
 import ChatEntityScreen from '../modules/entities/chat/chat-entity-screen'
@@ -41,8 +41,8 @@ export const REGISTER_SCREEN = 'nav.RegisterScreen'
 export const FORGOT_PASSWORD_SCREEN = 'nav.ForgotPasswordScreen'
 export const CHANGE_PASSWORD_SCREEN = 'nav.ChangePasswordScreen'
 export const SETTINGS_SCREEN = 'nav.SettingsScreen'
-export const PREFERENCES_SCREEN = 'nav.PreferencesScreen'
 export const PROFILE_SCREEN = 'nav.ProfileScreen'
+export const EVENT_INFO_SCREEN = 'nav.EventInfoScreen'
 export const LAUNCH_SCREEN = 'nav.LaunchScreen'
 export const DRAWER_CONTENT = 'nav.DrawerContent'
 export const ENTITIES_SCREEN = 'nav.EntitiesScreen'
@@ -154,8 +154,8 @@ export function registerScreensAndStartApp() {
   Navigation.registerComponentWithRedux(FORGOT_PASSWORD_SCREEN, () => ForgotPasswordScreen, Provider, store)
   Navigation.registerComponentWithRedux(CHANGE_PASSWORD_SCREEN, () => ChangePasswordScreen, Provider, store)
   Navigation.registerComponentWithRedux(SETTINGS_SCREEN, () => SettingsScreen, Provider, store)
-  Navigation.registerComponentWithRedux(PREFERENCES_SCREEN, () => PreferencesScreen, Provider, store)
   Navigation.registerComponentWithRedux(PROFILE_SCREEN, () => ProfileScreen, Provider, store)
+  Navigation.registerComponentWithRedux(EVENT_INFO_SCREEN, () => EventInfoScreen, Provider, store)
   Navigation.registerComponentWithRedux(DRAWER_CONTENT, () => DrawerContent, Provider, store)
   Navigation.registerComponentWithRedux(LAUNCH_SCREEN, () => LaunchScreen, Provider, store)
   Navigation.registerComponentWithRedux(ENTITIES_SCREEN, () => EntitiesScreen, Provider, store)
@@ -287,20 +287,6 @@ export const settingsScreen = () =>
       },
     },
   })
-export const preferencesScreen = () =>
-  Navigation.push('center', {
-    component: {
-      name: PREFERENCES_SCREEN,
-      options: {
-        topBar: {
-          title: {
-            text: 'Preferences',
-            color: Colors.snow,
-          },
-        },
-      },
-    },
-  })
 export const profileScreen = () =>
   Navigation.push('center', {
     component: {
@@ -309,6 +295,20 @@ export const profileScreen = () =>
         topBar: {
           title: {
             text: 'Profile',
+            color: Colors.snow,
+          },
+        },
+      },
+    },
+  })
+export const eventInfoScreen = () =>
+  Navigation.push('center', {
+    component: {
+      name: EVENT_INFO_SCREEN,
+      options: {
+        topBar: {
+          title: {
+            text: 'Event Info',
             color: Colors.snow,
           },
         },

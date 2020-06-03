6c8c534ee36e0a419af4d9f6d3a18747c5611f5a
KlaudiaK1998 <62907563+KlaudiaK1998@users.noreply.github.com>
2020-06-03 23:01:40 +0200

    Fix

--- mingle/app/modules/account/profile/profile-screen.js
+++ mingle/app/modules/account/profile/profile-screen.js
@@ -6,7 +6,6 @@ import styles from './profile-screen.styles'
 import AccountActions from '../../../shared/reducers/account.reducer'
 import RegisterActions from "../register/register.reducer";
 import {connect} from "react-redux";
-import ImagePicker from 'react-native-image-picker';
 
 
 //export default
@@ -66,32 +65,6 @@ class ProfileScreen extends React.Component {
 
   
   launchImageLibrary = () => {  //todo
-    let options = {
-      storageOptions: {
-        skipBackup: true,
-        path: 'images',
-      },
-    };
-    ImagePicker.launchImageLibrary(options, (response) => {
-      console.log('Response = ', response);
-
-      if (response.didCancel) {
-        console.log('User cancelled image picker');
-      } else if (response.error) {
-        console.log('ImagePicker Error: ', response.error);
-      } else if (response.customButton) {
-        console.log('User tapped custom button: ', response.customButton);
-        alert(response.customButton);
-      } else {
-        const source = { uri: response.uri };
-        console.log('response', JSON.stringify(response));
-        this.setState({
-          filePath: response,
-          fileData: response.data,
-          fileUri: response.uri
-        });
-      }
-    });
 
   }
 
